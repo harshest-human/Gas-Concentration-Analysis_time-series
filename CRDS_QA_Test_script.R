ea87960aa2f96d38e8b64f09868969c54f9b3153
Harsh Sahu <harshsahu.8085@gmail.com>
2024-11-13 12:53:02 +0100

    4 hourly trends for 1 week and diff color

--- CRDS_QA_Test_script.R
+++ CRDS_QA_Test_script.R
@@ -36,42 +36,46 @@ CRDS.long$DATE.TIME <- as.POSIXct(CRDS.long$DATE.TIME, format = "%Y-%m-%d %H:%M:
 ########### Import reshaped dataframe ############
 CRDS.long <- fread("2024_Nov_06_to_11_CRDS.long.csv")
 
+# Filter rows by 'DATE.TIME' range before merging
+CRDS.long$DATE.TIME <- as.POSIXct(CRDS.long$DATE.TIME, format="%Y-%m-%d %H:%M:%S")  
+CRDS.long <- CRDS.long[DATE.TIME >= "2024-11-06 14:00:00" & DATE.TIME <= "2024-11-11 10:00:00"]
+
 
 ########### Data processing (hourly averages) ############
-CRDS.long$sampling.point = as.factor(CRDS.long$sampling.point)
-
-CRDS.long$DATE.TIME <- as.POSIXct(CRDS.long$DATE.TIME, format="%Y-%m-%d %H:%M:%S")  
-
 # Extract the hour from the DATE.TIME column
-CRDS.long$Hour <- floor_date(CRDS.long$DATE.TIME, unit = "2 hours")
+CRDS.long$Hour <- floor_date(CRDS.long$DATE.TIME, unit = "4 hours")
 
 # Calculate the hourly average for each ID, sampling.point, and hour
-CRDS.long <- CRDS.long %>%
-        group_by(ID, sampling.point, Hour) %>%
-        summarise(
-                CO2 = mean(CO2, na.rm = TRUE),
+CRDS.long <- CRDS.long %>% group_by(ID, sampling.point, Hour) %>%
+        summarise(CO2 = mean(CO2, na.rm = TRUE),
                 CH4 = mean(CH4, na.rm = TRUE),
                 NH3 = mean(NH3, na.rm = TRUE),
                 H2O = mean(H2O, na.rm = TRUE))
 
 
 ########### Data Visualization ggplot2 ############
-# Plotting CO2 trends with DATE.TIME on x-axis, faceted by sampling.point and colored by ID
+CRDS.long$sampling.point = as.factor(CRDS.long$sampling.point)
+CRDS.long$ID = as.factor(CRDS.long$ID)
+
 ggplot(CRDS.long, aes(x = Hour, y = CO2, color = ID)) + 
         geom_line() +  
-        theme(legend.position = "bottom") 
+        scale_x_datetime(date_breaks = "4 hour", date_labels = "%Y-%m-%d %H:%M") +  
+        scale_color_manual(values = c("red", "blue")) +
+        theme(legend.position = "bottom",axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Plotting CH4 trends with DATE.TIME on x-axis, faceted by sampling.point and colored by ID
 ggplot(CRDS.long, aes(x = Hour, y = CH4, color = ID)) + 
         geom_line() +  
-        theme(legend.position = "bottom") 
+        scale_x_datetime(date_breaks = "2 hour", date_labels = "%Y-%m-%d %H:%M") +  
+        scale_color_manual(values = c("red", "blue")) +
+        theme(legend.position = "bottom",axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Plotting NH3 trends with DATE.TIME on x-axis, faceted by sampling.point and colored by ID
 ggplot(CRDS.long, aes(x = Hour, y = NH3, color = ID)) + 
         geom_line() +  
-        theme(legend.position = "bottom") 
-
-
+        scale_x_datetime(date_breaks = "2 hour", date_labels = "%Y-%m-%d %H:%M") +  
+        scale_color_manual(values = c("red", "blue")) +
+        theme(legend.position = "bottom",axis.text.x = element_text(angle = 45, hjust = 1))
 
 
 
