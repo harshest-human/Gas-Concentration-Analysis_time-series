e39fe7b0fa83b1e84ecdadcaadaa3b67ad406f5b
Harsh Sahu <harshsahu.8085@gmail.com>
2025-06-23 14:09:24 +0200

    Data Viz by ggline

--- Ringversuche_script.R
+++ Ringversuche_script.R
@@ -18,15 +18,61 @@ LUFA_CRDS <- read.csv("20250408-15_hourly_LUFA_CRDS.P8.csv")
 UB_CRDS <- read.csv("20250408-15_hourly_UB_CRDS.P8.csv")
 lab_combined <- bind_rows(ANECO_FTIR, MBBM_FTIR, ATB_FTIR, ATB_CRDS, LUFA_CRDS, UB_CRDS)
 
-####### Data Visualization ##########
-lab_combined.1 <- lab_combined %>% filter(DATE.TIME >= "2025-04-08 12:00:00" & DATE.TIME <= "2025-04-09 12:00:00")
-lab_combined.1 <- lab_combined %>% filter(analyzer != "FTIR.1")
+lab_combined$lab.anaylzer <- paste(lab_combined$lab, lab_combined$analyzer, sep = "_")
+lab_combined <- lab_combined %>% mutate(hour = hour(DATE.TIME))
+lab_combined$hour <- as.factor(lab_combined$hour)
+lab_combined$DATE.TIME <- as.POSIXct(lab_combined$DATE.TIME)
+lab_combined <- lab_combined %>% select(DATE.TIME, hour, location, lab.anaylzer, CO2, CH4, NH3)
+lab_combined <- lab_combined %>% filter(DATE.TIME >= "2025-04-08 12:00:00" & DATE.TIME <= "2025-04-15 12:00:00")
 
-ggplot(lab_combined.1, aes(x = DATE.TIME, y = CO2, color = lab)) +
-        geom_point() +
-        facet_wrap(~ location) +
-        labs(title = "CO2 (ppmv) by Lab over Time", x = "Hour", y = "CO2 (ppmv)") +
-        theme_minimal()
+
+####### Data Visualization of all data (1 week) ##########
+#CO2
+ggline(lab_combined,
+       x = "DATE.TIME",
+       y = "CO2",
+       add = "mean_se",
+       color = "lab.anaylzer",
+       facet.by = "location",
+       xlab = "Hour",
+       ylab = "CO2 (ppmv)",
+       title = "CO2 (ppmv) over Time (Mean ± SE)") +
+        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
+        scale_y_continuous(breaks = seq(0, 10000, by = 1)) +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+
+
+#CH4
+ggline(lab_combined,
+       x = "DATE.TIME",
+       y = "CH4",
+       add = "mean_se",
+       color = "lab.anaylzer",
+       facet.by = "location",
+       xlab = "Hour",
+       ylab = "CH4 (ppmv)",
+       title = "CH4 (ppmv) over Time (Mean ± SE)") +
+        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
+        scale_y_continuous(breaks = seq(0, 1000, by = 1)) +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+
+#NH3
+ggline(lab_combined,
+       x = "DATE.TIME",
+       y = "NH3",
+       add = "mean_se",
+       color = "lab.anaylzer",
+       facet.by = "location",
+       xlab = "Hour",
+       ylab = "NH3 (ppmv)",
+       title = "NH3 (ppmv) over Time (Mean ± SE)") +
+        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
+        scale_y_continuous(breaks = seq(0, 100, by = 0.1)) +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+
 
 
 
