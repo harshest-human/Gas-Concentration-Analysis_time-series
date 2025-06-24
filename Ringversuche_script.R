a22c8cdcd151d4a5200c263408252322d037ec06
Harsh Sahu <harshsahu.8085@gmail.com>
2025-06-25 00:06:55 +0200

    function plot_concentration and ggsave pdf

--- Ringversuche_script.R
+++ Ringversuche_script.R
@@ -7,6 +7,7 @@ library(psych)
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
+library(scales)
 
 ######## Import Gas Data #########
 #Load processed datasets
@@ -20,107 +21,85 @@ UB_CRDS <- read.csv("20250408-15_hourly_UB_CRDS.P8.csv")
 
 #combine all data set
 lab_combined <- bind_rows(LUFA_FTIR, ANECO_FTIR, MBBM_FTIR, ATB_FTIR, ATB_CRDS, LUFA_CRDS, UB_CRDS)
-lab_combined$lab.anaylzer <- paste(lab_combined$lab, lab_combined$analyzer, sep = "_")
+lab_combined$lab.analyzer <- paste(lab_combined$lab, lab_combined$analyzer, sep = "_")
 lab_combined$DATE.TIME <- as.POSIXct(lab_combined$DATE.TIME)
-lab_combined <- lab_combined %>% select(DATE.TIME, location, lab.anaylzer, CO2, CH4, NH3)
+lab_combined <- lab_combined %>% select(DATE.TIME, location, lab.analyzer, CO2, CH4, NH3)
 lab_combined <- lab_combined %>% filter(DATE.TIME >= "2025-04-08 12:00:00" & DATE.TIME <= "2025-04-15 12:00:00")
 lab_combined <- lab_combined %>% mutate(hour = hour(DATE.TIME))
 lab_combined$hour <- as.factor(lab_combined$hour)
 
 #write csv
 lab_combined <- lab_combined %>% select(DATE.TIME, hour, everything())
-write.csv(lab_combined, "20250408-15_Ringversuche_lab_combined_data.csv", row.names = FALSE)
+write.csv(lab_combined, "20250408-15_final_ringversuche_concentration_combined_data.csv", row.names = FALSE)
 
 ####### Data Visualization Weekly ##########
-#CO2
-ggline(lab_combined,
-       x = "DATE.TIME",
-       y = "CO2",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Date",
-       ylab = "CO2 (ppmv)",
-       title = "CO2 (ppmv) over Time (Mean ± SE)") +
-        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
-        scale_y_continuous(breaks = seq(0, 10000, by = 10)) +
-        theme_light() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+plot_concentration <- function(df, x, y) {
+        #load libraries
+        library(dplyr)
+        library(ggpubr)
+        library(ggplot2)
+        library(scales)
+        
+        # Check if x and y exist in the dataframe
+        if (!(x %in% colnames(df))) stop(paste("X-axis column", x, "not found in dataframe."))
+        if (!(y %in% colnames(df))) stop(paste("Y-axis column", y, "not found in dataframe."))
+        
+        # Filter out NA or non-finite values from y
+        df <- df %>% filter(!is.na(.data[[y]]), is.finite(.data[[y]]))
+        
+        # Start building plot
+        p <- ggline(df,
+                    x = x,
+                    y = y,
+                    add = "mean_se",
+                    color = "lab.analyzer",
+                    facet.by = "location") +
+                labs(
+                        title = paste(y, "Concentration Trends (Mean ± SE)"),
+                        x = x,
+                        y = paste(y, "(ppmv)"),
+                        color = "Laboratory"
+                ) +
+                theme_light() +
+                theme(legend.position = "top")
+        
+        # If x is datetime, add datetime scale and rotate x-axis labels
+        if (inherits(df[[x]], "POSIXct")) {
+                p <- p +
+                        scale_x_datetime(date_breaks = "6 hours", date_labels = "%d.%m %H:%M") +
+                        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+        }
+        
+        return(p)
+}
 
 
-#CH4
-ggline(lab_combined,
-       x = "DATE.TIME",
-       y = "CH4",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Date",
-       ylab = "CH4 (ppmv)",
-       title = "CH4 (ppmv) over Time (Mean ± SE)") +
-        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
-        scale_y_continuous(breaks = seq(0, 1000, by = 1)) +
-        theme_light() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+# --- Generate DATE.TIME plots ---
+plot_CO2_conc_all <- plot_concentration(lab_combined, x = "DATE.TIME", y = "CO2")
+plot_CH4_conc_all <- plot_concentration(lab_combined, x = "DATE.TIME", y = "CH4")
+plot_NH3_conc_all <- plot_concentration(lab_combined, x = "DATE.TIME", y = "NH3")
 
-#NH3
-ggline(lab_combined,
-       x = "DATE.TIME",
-       y = "NH3",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Date",
-       ylab = "NH3 (ppmv)",
-       title = "NH3 (ppmv) over Time (Mean ± SE)") +
-        scale_x_datetime(date_breaks = "24 hours", date_labels = "%d.%m", expand = c(0.1, 0.1))  +
-        scale_y_continuous(breaks = seq(0, 100, by = 0.1)) +
-        theme_light() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1, size =8))
+# --- View DATE.TIME plots ---
+plot_CO2_conc_all
+plot_CH4_conc_all
+plot_NH3_conc_all
 
+# --- Save DATE.TIME plots ---
+ggsave("plot_CO2_conc_all.pdf", plot_CO2_conc_all, width = 10, height = 6)
+ggsave("plot_CH4_conc_all.pdf", plot_CH4_conc_all, width = 10, height = 6)
+ggsave("plot_NH3_conc_all.pdf", plot_NH3_conc_all, width = 10, height = 6)
 
-####### Data Visualization Hourly ##########
-#CO2
-ggline(lab_combined,
-       x = "hour",
-       y = "CO2",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Hour",
-       ylab = "CO2 (ppmv)",
-       title = "CO2 (ppmv) over Time (Mean ± SE)") +
-        scale_y_continuous(breaks = seq(0, 10000, by = 10)) +
-        theme_light()
+# --- Generate hour-based plots ---
+plot_CO2_conc_hour <- plot_concentration(lab_combined, x = "hour", y = "CO2")
+plot_CH4_conc_hour <- plot_concentration(lab_combined, x = "hour", y = "CH4")
+plot_NH3_conc_hour <- plot_concentration(lab_combined, x = "hour", y = "NH3")
 
+# --- View hour-based plots ---
+plot_CO2_conc_hour
+plot_CH4_conc_hour
+plot_NH3_conc_hour
 
-#CH4
-ggline(lab_combined,
-       x = "hour",
-       y = "CH4",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Hour",
-       ylab = "CH4 (ppmv)",
-       title = "CH4 (ppmv) over Time (Mean ± SE)") +
-        scale_y_continuous(breaks = seq(0, 10000, by = 10)) +
-        theme_light()
-
-
-#NH3
-ggline(lab_combined,
-       x = "hour",
-       y = "NH3",
-       add = "mean_se",
-       color = "lab.anaylzer",
-       facet.by = "location",
-       xlab = "Hour",
-       ylab = "NH3 (ppmv)",
-       title = "NH3 (ppmv) over Time (Mean ± SE)") +
-        scale_y_continuous(breaks = seq(0, 10000, by = 0.1)) +
-        theme_light()
-
-
-
-
+# --- Save hour-based plots ---
+ggsave("plot_CO2_conc_hour.pdf", plot_CO2_conc_hour, width = 10, height = 6)
+ggsave("plot_CH4_conc_hour.pdf", plot_CH4_conc_hour, width = 10, height = 6)
+ggsave("plot_NH3_conc_hour.pdf", plot_NH3_conc_hour, width = 10, height = 6)
